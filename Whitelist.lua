7e84e11e17bd11c3e0143a236fb4bb69f29d6b48
OzuGod1 <90089711+OzuGod1@users.noreply.github.com>
2024-06-15 01:25:17 +0700

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -4,7 +4,7 @@ repeat task.wait() until game:IsLoaded()
 repeat task.wait() until game.Players.LocalPlayer
 if game.PlaceId ~= 17017769292 then return end
 warn("=========================================")
-warn("=========== Google Sheet v1.1 ===========")
+warn("=========== Google Sheet v1.2 ===========")
 warn("=========================================")
 local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
@@ -36,11 +36,25 @@ local function userExists(data)
     return false
 end
 
+local function getItems()
+    local items = {}
+    for i, v in next, getgc(true) do
+        if typeof(v) == "table" then
+            local crystalValue = rawget(v, "Trait Crystal")
+            if typeof(crystalValue) == "number" then
+                items = v
+            end
+        end
+    end
+    return items
+end
+
 local function getUserData()
     return {
         Username = Player.Name,
         Level = PlayerGui.Toolbar.XPBar.XPAmount.Text:match("Level (%d+)"),
-        Gems = PlayerGui.Toolbar.CurrencyList.Gems.TextLabel.Text:gsub(",", "")
+        Gems = PlayerGui.Toolbar.CurrencyList.Gems.TextLabel.Text:gsub(",", ""),
+        TraitCrystal = getItems()["Trait Crystal"]
     }
 end
 
@@ -63,7 +77,7 @@ end
 
 local function createUser()
     local userData = getUserData()
-    sendRequest("?action=insert&user=" .. userData.Username .. "&level=" .. userData.Level .. "&gems=" .. userData.Gems .. "&rr=1")
+    sendRequest("?action=insert&user=" .. userData.Username .. "&level=" .. userData.Level .. "&gems=" .. userData.Gems .. "&rr=" .. userData.TraitCrystal)
 end
 
 local function updateUser()
